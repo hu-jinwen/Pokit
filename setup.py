8ae2da06c93caf5276b3e921411a1a19ffcfce3f
hujinwen <hujinwen@shizhuang-inc.com>
2021-03-03 20:09:49 +0800

    修改setup文件

--- setup.py
+++ setup.py
@@ -6,7 +6,7 @@ from setuptools import setup
 
 setup(
     name='pokit',
-    version='0.1.9',
+    version='0.1.10',
     description='python toolkit',
     url='https://github.com/hu-jinwen/Pokit',
     author='hu-jinwen',
